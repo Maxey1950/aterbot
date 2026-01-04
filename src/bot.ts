baa60782531eccf1e7b6cd9032aab89f859ae8f1
Maxey <92372504+Maxey1950@users.noreply.github.com>
2026-01-03 20:06:12 -0500

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,16 @@
 import Mineflayer from 'mineflayer';
 import { sleep, getRandom } from "./utils.ts";
-import CONFIG from "../config.json" assert {type: 'json'};
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONFIG = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../config.json"), "utf8")
+);
+
 
 let loop: NodeJS.Timeout;
 let bot: Mineflayer.Bot;
